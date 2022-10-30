42f69243280b541c32cb7e3a1672b04c46b16307
Tushar <tusharsrivastava162@gmail.com>
2022-10-30 15:06:48 +0530

    new vs init cleanup

--- python/new_vs_init.py
+++ python/new_vs_init.py
@@ -10,27 +10,3 @@ class User:
 
 u = User('Tushar')
 print(u.__dict__)
-
-
-x = 0
-
-
-# scope resolution
-def f1():
-    global x
-    lg.info('')
-
-    x = 1
-    
-    def f2():
-        lg.info('f2')
-        nonlocal x  # write, not for read
-        lg.info(x)
-        x = 2
-        lg.info(x)
-    
-    lg.info(x)
-    f2()
-    lg.info(x)
-
-f1()
